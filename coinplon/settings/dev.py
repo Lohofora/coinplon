30832201657328ab6a2020adf391abcbc22e56d6
Lohofora <marion2.studer@laposte.net>
2022-10-10 14:12:41 +0200

    first commit

---
+++ coinplon/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&o2yaf!667e*5kp%5&2h&)k5e+h%54*l%xjy(2b)+-$=^(!z-*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
